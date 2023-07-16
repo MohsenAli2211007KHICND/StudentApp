fa78d70a345c8ce3bf8ef90691766d95fcc2f8c2
Mohsen Ali <mohsen.ali@xloopdigital.com>
2023-07-16 15:04:35 +0500

    Add project files.

---
+++ StudentApp/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentApp.Models;
+using System.Net;
+
+namespace StudentApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all", Name = "GetStudents")]
+        public IEnumerable<Student> GetStudents()
+        {
+            return StudentRepository.Students;
+        }
+
+        [HttpGet("{id:int}", Name = "GetStudentById")]
+        public Student GetStudentById(int id)
+        {
+            return StudentRepository.Students.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpGet]
+        [Route("{name}", Name = "GetStudentByName")]
+        public Student GetStudentByName (string name)
+        {
+            return StudentRepository.Students.FirstOrDefault(u => u.StudentName == name);
+        }
+
+        [HttpDelete]
+        [Route("{id}", Name = "DeleteStudent")]
+        public bool DeleteStudent(int id)
+        {
+            var student = StudentRepository.Students.FirstOrDefault(u => u.Id == id);
+            if (student != null)
+            {
+                StudentRepository.Students.Remove(student);
+            }
+            return true;
+        }
+    }
+}
---
+++ StudentApp/StudentRepository.cs
@@ -0,0 +1,24 @@
+﻿using StudentApp.Models;
+
+namespace StudentApp
+{
+    public static class StudentRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student> { new Student
+            {
+                Id = 1,
+                StudentName = "Mohsen Ali",
+                Email = "mohsenali@work.com",
+                Address = "Buleda, Turbat, Balochistan",
+                Age = 23
+            },
+             new Student{
+                Id = 2,
+                StudentName = "Faiz Gul",
+                Email = "faizgul@work.com",
+                Address = "Buleda, Turbat, Balochistan",
+                Age = 24
+            }
+            };
+    }
+}
